2ae0790b4dacfdc7dfd800c44ffeb25cbd68a582
Alexander Mogavero <mogsten@outlook.com>
2016-01-15 09:41:41 +1030

    Initial Commit

---
+++ Alexander Mogavero - WWDC 2016 Application/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Alexander Mogavero - WWDC 2016 Application
+//
+//  Created by Alexander Mogavero on 15/01/2016.
+//  Copyright © 2016 Alexander Mogavero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Alexander Mogavero - WWDC 2016 ApplicationTests/Alexander_Mogavero___WWDC_2016_ApplicationTests.swift
@@ -0,0 +1,36 @@
+//
+//  Alexander_Mogavero___WWDC_2016_ApplicationTests.swift
+//  Alexander Mogavero - WWDC 2016 ApplicationTests
+//
+//  Created by Alexander Mogavero on 15/01/2016.
+//  Copyright © 2016 Alexander Mogavero. All rights reserved.
+//
+
+import XCTest
+@testable import Alexander_Mogavero___WWDC_2016_Application
+
+class Alexander_Mogavero___WWDC_2016_ApplicationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
